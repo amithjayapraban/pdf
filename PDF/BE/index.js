97666eea77cc9c084046f917ed0562c4396b8360
Amith Jayapraban <89343885+amithjayapraban@users.noreply.github.com>
2023-12-17 14:53:48 +0530

    Update index.js

--- PDF/BE/index.js
+++ PDF/BE/index.js
@@ -10,6 +10,12 @@ let corsOptions = {
    origin : ['https://pdf-be-rs3l.onrender.com'], 
 } 
 app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://pdfv.vercel.app');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 const storage = multer.memoryStorage();
 const temp = multer({ storage });
